bf9d5f0923d6510b172a3f49319ce0916efb398e
muhligs <muhligs@users.noreply.github.com>
2019-06-28 08:44:26 +0200

    Add files via upload

---
+++ utils.R
@@ -0,0 +1,17 @@
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+# adding environment
+utensiles <- new.env(parent = globalenv())
+source("/home/mmn/Dropbox/programming/R/tools/here.R",local = utensiles)
+source("/home/mmn/Dropbox/programming/R/tools/colsplit.R",local = utensiles)
+source("/home/mmn/Dropbox/programming/R/tools/splitup.R",local = utensiles)
+source("/home/mmn/Dropbox/programming/R/tools/heat.R",local = utensiles)
+source("/home/mmn/Dropbox/programming/R/tools/overlap.R",local = utensiles)
+source("/home/mmn/Dropbox/programming/R/tools/is.unique.R",local = utensiles)
+source("/home/mmn/Dropbox/programming/R/tools/find.R",local = utensiles)
+source("/home/mmn/Dropbox/programming/R/tools/clipboard.R",local = utensiles)
+source("/home/mmn/Dropbox/programming/R/tools/gif.R",local = utensiles)
+source("/home/mmn/Dropbox/programming/R/tools/qq.p.R",local = utensiles)
+attach(utensiles)
+
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
