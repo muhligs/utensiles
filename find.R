67c058839ffdcdbafb483ab8b5d43ae34b689895
muhligs <muhligs@users.noreply.github.com>
2019-06-28 08:35:57 +0200

    Update find.R

--- find.R
+++ find.R
@@ -1,5 +1,5 @@
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-find <- function(x,loc = "/home/mmn/Dropbox/programming/R"){
+find <- function(x,loc = "/path/to/my/Rscripts"){
 	if(grepl(" ",x)&!grepl("^'",x))x <- sub("$","'",sub("^","'",x))
   var <- paste0("grep -r --include=*.R --include=*.Rmd --include=*.txt -A 1 -B 1 ",x," ",loc," &")
   cat(var,"\n")
